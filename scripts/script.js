2df09388473caf4f019204549be6e462d3da9e83
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-09-05 22:15:09 +0100

    some advance validation checks with javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+// const loginForm             = document.querySelector("#login");
+// const createAccountForm     = document.querySelector("#createAccount");
+// const submitBtn     = document.querySelector(".form__button");
+// const userUserName          = document.querySelector("#register");
 
+
+
+
+function setFormMessage(formElement, type, message) {
+
+    const messageElement = formElement.querySelector(".form__message");
+    
     messageElement.textContent = message;
     messageElement.classList.remove("form__message--success", "form__message--error");
     messageElement.classList.add(`form__message--${type}`);
@@ -21,39 +30,37 @@ function clearInputError(inputElement) {
 
 
 
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
+    const loginForm             = document.querySelector("#login");
+    const userUserName          = document.querySelector("#register");
 
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
+    loginForm.addEventListener("submit", (e)=> {
         e.preventDefault();
 
         // Perform your AJAX/Fetch login
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
+        try {
+            throw Error("Invalid username/password combination 🔑🗝🔑")
+        } catch (error) {
+            setFormMessage(loginForm, "error", error);
+        }
+       
     });
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-        });
 
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
+    userUserName.addEventListener("blur", (e)=> {
+        e.preventDefault()
+        if (userUserName.id == "register" && userUserName.value.length > 0 && userUserName.value.length < 10){
+            setInputError(userUserName, "Username must be at least 10 characters in length ❌");
+        }
     });
+    
+    userUserName.addEventListener("input", ()=> {
+        clearInputError(userUserName);
+    });
+    
+
 });
